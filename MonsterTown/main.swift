7c074ad79fc3e0e33eafc0acad930033a36c0696
Felicia Weathers <fel.weathers@gmail.com>
2016-02-09 23:41:54 -0500

    Initial Commit

---
+++ MonsterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Felicia Weathers on 2/9/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
